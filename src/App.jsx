83777aed92ce73f87ae57cbaee86986aeaf70ae6
Matias Mansilla <matiazep2@gmail.com>
2024-02-28 11:30:26 -0300

    Actulizacion antes del parcial

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 
 import './App.css'
-
+import Form from './Components/Form'
 function App() {
   
 
   return (
     <div className='App'>
+      <h1>Carga de estudiantes</h1>
+      <Form/>
       <Card/>
-      <form>
-        
-      </form>
+      
     </div>
   )
 }
